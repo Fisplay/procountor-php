e3cc9b93d22f09b7b88b089336656e063cb01f26
Jaakko Mantila <jaakko.mantila@fisplay.com>
2020-11-19 15:21:07 +0200

    updated phpunit 7, guzzle 7.0.1, php 7.2

--- tests/Procountor/ClientTest.php
+++ tests/Procountor/ClientTest.php
@@ -34,15 +34,17 @@ class ClientTest extends TestCase
         $client = new Client($logger);
 
         $client->request('localhost', 'POST', '{"Content-Type": "application/json"}', '{a: b}');
-        $this->assertArraySubset([
+        $requestLog = $logger->log[0];
+        unset($requestLog[5]);
+        $this->assertEquals([
             0 => 'localhost',
             1 => 'POST',
             2 => '{"Content-Type": "application/json"}',
             3 => '{a: b}',
             4 => '404',
-            //'{"Server":["nginx"],"Date":["Thu, 05 Apr 2018 06:28:21 GMT"],"Content-Type":["text\/html;charset=utf-8"],"Content-Length":["1105"],"Connection":["keep-alive"],"Content-Language":["en"]}',
+            // 5 => '{"Server":["nginx"],"Date":["Thu, 05 Apr 2018 06:28:21 GMT"],"Content-Type":["text\/html;charset=utf-8"],"Content-Length":["1105"],"Connection":["keep-alive"],"Content-Language":["en"]}',
             6 => '{}'
-        ], $logger->log[0]);
+        ], $requestLog);
 
         $responseHeaders = json_decode($logger->log[0][5], true);
         $this->assertEquals('nginx', $responseHeaders['Server'][0]);
