69ce45045b4ce538a170ffa820b1ed8a317da979
Petteri Hakala <petteri.hakala@fisplay.com>
2021-02-01 08:52:03 +0200

    Remove magic setter. Considering it as not that good practice.

--- src/Procountor/Response/AbstractResponse.php
+++ src/Procountor/Response/AbstractResponse.php
@@ -9,10 +9,4 @@ abstract class AbstractResponse {
     public function __construct(stdClass $data) {
         $this->data = $data;
     }
-
-    public function __set(string $attribute, $value) {
-        if (isset($this->data->$attribute)) {
-            $this->data->$attribute = $value;
-        }
-    }
 }
