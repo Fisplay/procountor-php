c8e958123923d64a235c1a67da68c019cf201694
Iikka Timlin <iikka@docue.com>
2021-09-23 13:52:49 +0300

    [WIP] Refactoring client

--- src/Procountor/Client.php
+++ src/Procountor/Client.php
@@ -2,54 +2,42 @@
 
 namespace Procountor\Procountor;
 
-use GuzzleHttp\Client as GuzzleClient;
 use Procountor\Procountor\Interfaces\AbstractResourceInterface;
-use Procountor\Procountor\Interfaces\LoggerInterface;
 use Procountor\Procountor\Json\Builder;
+use Psr\Http\Client\ClientInterface;
+use Psr\Http\Message\RequestFactoryInterface;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\StreamFactoryInterface;
+use Psr\Log\LoggerInterface;
 
 class Client
 {
-    private $accessToken;
-    private $mode = 'prod';
-    private $state = null;
-    private $loginParameters = [];
-    private $guzzleClient;
-    private $debug = false;
-    private $logger = null;
-    private $apiVersion = 'latest';
 
-    public function __construct(LoggerInterface $logger)
-    {
-        $this->guzzleClient = new GuzzleClient(['base_uri' => $this->getBaseUri()]);
-        $this->state = rand() . strtotime('now');
+    public const ENV_PRODUCTION = 'production';
+    public const ENV_DEVELOPMENT = 'development';
+
+    private ClientInterface $httpClient;
+    private RequestFactoryInterface $requestFactory;
+    private StreamFactoryInterface $streamFactory;
+    private LoggerInterface $logger;
+    private string $apiVersion;
+    private string $mode;
+    private string $accessToken;
+    private array $loginParameters;
+
+
+    public function __construct(
+        ClientInterface $httpClient,
+        RequestFactoryInterface $requestFactory,
+        StreamFactoryInterface $streamFactory,
+        LoggerInterface $logger,
+        string $apiVersion = 'latest',
+        string $mode = self::ENV_DEVELOPMENT
+    ) {
+        $this->httpClient = $httpClient;
+        $this->requestFactory = $requestFactory;
         $this->logger = $logger;
-    }
-
-    /**
-     * @deprecated
-     *
-     * Use authenticateByApiKey
-     */
-    public function login(
-        string $clientId,
-        string $clientSecret,
-        string $redirectUri,
-        string $username,
-        string $password,
-        int $company
-    ): self {
-        $this->loginParameters = [
-            'clientId' => $clientId,
-            'clientSecret' => $clientSecret,
-            'redirectUri' => $redirectUri,
-            'username' => $username,
-            'password' => $password,
-            'company' => $company,
-        ];
-
-        $code = $this->getAuthorizationCode();
-        $this->accessToken = $this->getAccessTokenByAuthorizationCode($code);
-        return $this;
+        $this->apiVersion = $apiVersion;
     }
 
     public function authenticateByApiKey(
@@ -60,26 +48,15 @@ class Client
         int $company
     ): self {
         $this->loginParameters = [
-            'clientId' => $clientId,
+            'clientId'     => $clientId,
             'clientSecret' => $clientSecret,
-            'redirectUri' => $redirectUri,
-            'apiKey' => $apiKey,
-            'company' => $company,
+            'redirectUri'  => $redirectUri,
+            'apiKey'       => $apiKey,
+            'company'      => $company,
         ];
 
         $this->accessToken = $this->getAccessTokenByApiKey($apiKey);
         return $this;
-    }
-
-
-    private function getRequestAuthHeaders()
-    {
-        $headers = [
-            'Content-Type' => 'application/json',
-            'Authorization' => 'Bearer ' . $this->accessToken
-        ];
-
-        return $headers;
     }
 
     public function post(string $resourceName, AbstractResourceInterface $resource)
@@ -92,19 +69,11 @@ class Client
         return $this->createRequest('PUT', $resourceName, $resource);
     }
 
-    public function request(string $url, string $type, string $headers, string $data)
+    public function request(RequestInterface $request, string $headers, string $data)
     {
+        $request->withHeader()
         $params = [
             'headers' => json_decode($headers, true),
-            'http_errors' => false,
-            'debug' => $this->debug,
-            'allow_redirects' => [
-                'max'             => 0,        // allow at most 10 redirects.
-                'strict'          => true,      // use "strict" RFC compliant redirects.
-                'referer'         => true,      // add a Referer header
-                'protocols'       => ['http'], // only allow https URLs
-                'track_redirects' => false
-            ],
         ];
 
         if ($params['headers']['Content-Type'] === 'application/json') {
@@ -113,7 +82,7 @@ class Client
             $params['form_params'] = json_decode($data, true);
         }
 
-        $request = $this->guzzleClient->request($type, $url, $params);
+        $request = $this->httpClient->sendRequest($type, $url, $params);
 
         $this->logger->log(
             $url,
@@ -129,46 +98,27 @@ class Client
         return $request;
     }
 
-    private function createRequest($type, string $resourceName, AbstractResourceInterface $resource = null)
+    public function createRequest(string $method, string $resourceName, ?AbstractResourceInterface $resource = null): RequestInterface
     {
-        $requestBody = [];
+        $request = $this->requestFactory->createRequest($method, $this->getResourceUrl($resourceName));
         if ($resource) {
             $builder = new Builder();
             $builder->setResource($resource);
-            $requestBody = $builder->getArray();
-            //echo json_encode($requestBody);
+            $requestBody = $builder->getJson();
+            $request = $request->withBody($this->streamFactory->createStream($requestBody));
         }
-
-        $request = $this->request($this->getResourceUrl($resourceName), $type, json_encode($this->getRequestAuthHeaders()), json_encode($requestBody));
-        /*if($request->getStatusCode()!=200) {
-            //var_dump($requestBody, $request->getStatusCode());
-            $error = new \stdClass();
-            $error->error = $request->getStatusCode();
-            $error->error_description = $request->getStatusCode();
-            $this->error($error);
-        }*/
-        $response = json_decode($request->getBody());
-
-        if (!empty($response->errors)) {
-            $this->error($response);
-        }
-
-        if (!empty($response->constraintViolations)) {
-            $error = new \stdClass();
-            $error->errors = [['message' =>  $response->constraintViolations[0]->errorCode]];
-            $this->error($error);
-        }
-
-        return $response;
+        return $request
+            ->withHeader('Content-Type', 'application/json')
+            ->withHeader('Authorization', sprintf('Bearer %s', $this->accessToken));
     }
 
     public function get(string $resourceName)
     {
-        //$response = $this->guzzleClient->request('GET', $this->getResourceUrl($resourceName), $this->getRequestAuthHeaders())->getBody();
+        //$response = $this->httpClient->request('GET', $this->getResourceUrl($resourceName), $this->getRequestAuthHeaders())->getBody();
         return $this->createRequest('GET', $resourceName);
     }
 
-    private function getResourceUrl(string $resourceName, $id = null): string
+    private function getResourceUrl(string $resourceName): string
     {
         return sprintf('%s/%s', $this->getBaseUri(), $resourceName);
     }
@@ -200,137 +150,24 @@ class Client
         return $result->access_token;
     }
 
-    private function getAccessTokenByAuthorizationCode(string $code): string
-    {
-
-        $post = [
-            'code' => $code,
-            'client_id' => $this->loginParameters['clientId'],
-            'client_secret' => $this->loginParameters['clientSecret'],
-            'redirect_uri' => $this->loginParameters['redirectUri'],
-        ];
-        $url = sprintf(
-            '%s?grant_type=authorization_code&',
-            $this->getUrlAccessToken()
-        ) . http_build_query($post);
-
-        $headers = [
-            'Content-Type' => 'application/x-www-form-urlencoded',
-        ];
-
-        $request = $this->request($url, 'POST', json_encode($headers), json_encode($post));
-        $result = json_decode($request->getBody());
-
-        if (!empty($result->errors)) {
-            $this->error($result);
-        }
-        return $result->access_token;
-    }
-
     private function error($result)
     {
         throw new ClientException($result->errors[0]->message);
     }
 
-    private function getAuthorizationCode(): string
-    {
-        $url =  sprintf(
-            '%s?response_type=code&client_id=%s&state=%s',
-            $this->getUrlAuthorize(),
-            $this->loginParameters['clientId'],
-            $this->getState()
-        );
-        $headers = [
-            'Content-Type' => 'application/x-www-form-urlencoded',
-        ];
-
-        $data = [
-            'response_type' => 'code',
-            'username' => urlencode($this->loginParameters['username']),
-            'password' => urlencode($this->loginParameters['password']),
-            'company' => urlencode($this->loginParameters['company']),
-            'redirect_uri' => $this->loginParameters['redirectUri'],
-        ];
-
-        $request = $this->request($url, 'POST', json_encode($headers), json_encode($data));
-
-
-        $result = $request->getBody();
-        $headers = $request->getHeaders();
-
-
-        if ($this->debug) {
-            echo "---------------------------------\n\n\nForm_params: ";
-            var_dump(http_build_query($data));
-            echo "\n";
-        }
-
-
-
-        if (!empty($headers['Location'])) {
-            $urlParsed = parse_url($headers['Location'][0]);
-            parse_str($urlParsed['query'], $queryParams);
-            return $queryParams['code'];
-        }
-
-        //If no location header, it must be error
-        preg_match('/(\{.*\})/', $result, $match);
-        $result = json_decode($match[1]);
-        $this->error($result ?: (object)['errors' => [['message' => 'API ERROR']]]);
-    }
-
-    public function setModeDev(): self
-    {
-        $this->mode = 'dev';
-        $this->guzzleClient = new GuzzleClient(['base_uri' => $this->getBaseUri()]);
-
-        return $this;
-    }
-
-    private function getUrlAuthorize(): string
-    {
-        return $this->getBaseUri() . $this->getUrls()[$this->mode]['urlAuthorize'];
-    }
-
     private function getUrlAccessToken(): string
     {
-        return $this->getBaseUri() . $this->getUrls()[$this->mode]['urlAccessToken'];
-    }
-
-    private function getState(): string
-    {
-        return $this->state;
+        return $this->getBaseUri() . '/oauth/token';
     }
 
     private function getBaseUri(): string
     {
-        return $this->getUrls()[$this->mode]['urlBase'];
-    }
-
-    public function setDebug(bool $debug)
-    {
-        $this->debug = $debug;
-    }
-
-    public function setApiVersion(string $apiVersion): self
-    {
-        $this->apiVersion = $apiVersion;
-        return $this;
-    }
-
-    private function getUrls(): array
-    {
-        return [
-            'prod' => [
-                'urlBase' => 'https://api.procountor.com/' . $this->apiVersion . '/api',
-                'urlAuthorize' => '/oauth/authz',
-                'urlAccessToken' => '/oauth/token',
-            ],
-            'dev' => [
-                'urlBase' => 'https://api-test.procountor.com/' . $this->apiVersion . '/api',
-                'urlAuthorize' => '/oauth/authz',
-                'urlAccessToken' => '/oauth/token',
-            ],
-        ];
+        switch ($this->mode) {
+            case self::ENV_PRODUCTION:
+                return 'https://api.procountor.com/' . $this->apiVersion . '/api';
+            case self::ENV_DEVELOPMENT:
+            default:
+                return 'https://api-test.procountor.com/' . $this->apiVersion . '/api';
+        }
     }
 }
