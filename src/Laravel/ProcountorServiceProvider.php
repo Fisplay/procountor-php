9b4ee07ad8dcec0c6968976172eb81bf29529eca
Iikka Timlin <iikka@docue.com>
2021-09-24 16:42:00 +0300

    Fix service provider

--- src/Laravel/ProcountorServiceProvider.php
+++ src/Laravel/ProcountorServiceProvider.php
@@ -2,11 +2,31 @@
 
 namespace Procountor\Laravel;
 
+use App\Console\Commands\Authorize;
+use Illuminate\Contracts\Support\DeferrableProvider;
 use Illuminate\Support\ServiceProvider;
 use Procountor\Procountor\Client;
+use Procountor\Procountor\Environment;
+use Psr\Cache\CacheItemPoolInterface;
+use Psr\Http\Client\ClientInterface;
+use Psr\Http\Message\RequestFactoryInterface;
+use Psr\Http\Message\StreamFactoryInterface;
+use Psr\Http\Message\UriFactoryInterface;
+use Psr\Log\LoggerInterface;
 
-class ProcountorServiceProvider extends ServiceProvider
+class ProcountorServiceProvider extends ServiceProvider implements DeferrableProvider
 {
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return [Client::class];
+    }
+
     /**
      * Bootstrap the application services.
      */
@@ -14,10 +34,10 @@ class ProcountorServiceProvider extends ServiceProvider
     {
         if ($this->app->runningInConsole()) {
             $this->publishes([
-                __DIR__ . '/../config/procountor.php' => config_path('procountor.php'),
+                __DIR__ . '/../../config/procountor.php' => config_path('procountor.php'),
             ], 'config');
             $this->commands([
-                // TODO
+                Authorize::class,
             ]);
         }
     }
@@ -28,8 +48,21 @@ class ProcountorServiceProvider extends ServiceProvider
     public function register()
     {
         $this->mergeConfigFrom(__DIR__ . '/../config/procountor.php', 'procountor');
-        $this->app->singleton(Client::class, fn () => new Client(
-            // TODO ....
+        $this->app->singleton(Client::class, fn ($app) => new Client(
+            $app->make(ClientInterface::class),
+            $app->make(RequestFactoryInterface::class),
+            $app->make(StreamFactoryInterface::class),
+            $app->make(LoggerInterface::class),
+            new Environment(
+                config('procountor.client_id'),
+                config('procountor.client_secret'),
+                config('procountor.api_key'),
+                config('procountor.base_uri'),
+                config('procountor.redirect_uri'),
+                $app->make(UriFactoryInterface::class),
+                config('procountor.api_version', 'latest')
+            ),
+            $app->make(CacheItemPoolInterface::class)
         ));
     }
 }
