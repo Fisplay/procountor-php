b4027e42c55da8cd71361b59af4166b90ab077ba
Iikka Timlin <iikka@docue.com>
2021-09-24 12:47:11 +0300

    Fix client init

--- tests/ApiTestCase.php
+++ tests/ApiTestCase.php
@@ -6,8 +6,10 @@ use Dotenv\Dotenv;
 use GuzzleHttp\Client as GuzzleHttpClient;
 use PhpExtended\HttpMessage\RequestFactory;
 use PhpExtended\HttpMessage\StreamFactory;
+use PhpExtended\HttpMessage\UriFactory;
 use PHPUnit\Framework\TestCase;
 use Procountor\Procountor\Client;
+use Procountor\Procountor\Environment;
 use Psr\Log\NullLogger;
 
 class ApiTestCase extends TestCase
@@ -17,19 +19,23 @@ class ApiTestCase extends TestCase
         $dotenv = Dotenv::createImmutable(__DIR__ . '/..', '.env.testing');
         $dotenv->load();
 
+        $prcountorEnv = new Environment(
+            $_ENV['PROCOUNTOR_CLIENT_ID'],
+            $_ENV['PROCOUNTOR_CLIENT_SECRET'],
+            $_ENV['PROCOUNTOR_API_KEY'] ?? null,
+            $_ENV['PROCOUNTOR_BASE_URI'],
+            $_ENV['PROCOUNTOR_REDIRECT_URI'],
+            new UriFactory()
+        );
+
         $client = new Client(
             new GuzzleHttpClient(),
             new RequestFactory(),
             new StreamFactory(),
-            new NullLogger()
+            new NullLogger(),
+            $prcountorEnv
         );
 
-        return $client->authenticateByApiKey(
-            $_ENV['PROCOUNTOR_CLIENT_ID'],
-            $_ENV['PROCOUNTOR_CLIENT_SECRET'],
-            $_ENV['PROCOUNTOR_REDIRECT_URI'],
-            $_ENV['PROCOUNTOR_API_KEY'],
-            $_ENV['PROCOUNTOR_COMPANY']
-        );
+        return $client->authenticateByApiKey();
     }
 }
