2472097d288faae19bbf45a47f4873bb9bf898f5
Iikka Timlin <iikka@docue.com>
2021-09-27 15:44:23 +0300

    ExtraInfo dockblocks

--- src/Procountor/Interfaces/ExtraInfoInterface.php
+++ src/Procountor/Interfaces/ExtraInfoInterface.php
@@ -2,11 +2,28 @@
 
 namespace Procountor\Procountor\Interfaces;
 
+
+/**
+ * interface ExtraInfoInterface
+ *
+ * @see Procountor\Procountor\Interfaces\ExtraInfoInterface
+ * @see https://dev.procountor.com/api-reference/#model-ExtraInfo
+ * @package Procountor\Procountor\Interfaces
+ */
 interface ExtraInfoInterface extends AbstractResourceInterface
 {
-    //Indicates if accounting by row is used (true) or not (false). Accounting by row means that a separate ledger transaction is created for each invoice row. ,
+    /**
+     * Indicates if accounting by row is used (true) or not (false).
+     * Accounting by row means that a separate ledger transaction is created for each invoice row.
+     *
+     * @return bool
+     */
     public function getAccountingByRow(): bool;
 
-    //Indicates if the unit prices on invoice rows include VAT (true) or not (false).
+    /**
+     * Indicates if the unit prices on invoice rows include VAT (true) or not (false).
+     *
+     * @return bool
+     */
     public function getUnitPricesIncludeVat(): bool;
 }
--- src/Procountor/Response/ExtraInfo.php
+++ src/Procountor/Response/ExtraInfo.php
@@ -3,18 +3,24 @@
 namespace Procountor\Procountor\Response;
 
 use Procountor\Procountor\Interfaces\ExtraInfoInterface;
-use stdClass;
 
+
+/**
+ * class ExtraInfo
+ *
+ * @see Procountor\Procountor\Interfaces\ExtraInfoInterface
+ * @see https://dev.procountor.com/api-reference/#model-ExtraInfo
+ *
+ * @package Procountor\Procountor\Response
+ */
 class ExtraInfo extends AbstractResponse implements ExtraInfoInterface
 {
 
-    //Indicates if accounting by row is used (true) or not (false). Accounting by row means that a separate ledger transaction is created for each invoice row. ,
     public function getAccountingByRow(): bool
     {
         return $this->data->accountingByRow;
     }
 
-    //Indicates if the unit prices on invoice rows include VAT (true) or not (false).
     public function getUnitPricesIncludeVat(): bool
     {
         return $this->data->unitPricesIncludeVat;
