455fb7a693c337f15f1fbc6cc912f0529737ed2d
Iikka Timlin <iikka@docue.com>
2021-09-27 14:43:39 +0300

    Dockblocks

--- src/Procountor/Interfaces/PaymentInfoInterface.php
+++ src/Procountor/Interfaces/PaymentInfoInterface.php
@@ -4,23 +4,87 @@ namespace Procountor\Procountor\Interfaces;
 
 use DateTime;
 
+
+/**
+ * interface PaymentInfoInterface
+ *
+ * @see https://dev.procountor.com/api-reference/#model-PaymentInfo
+ *
+ * @package Procountor\Procountor\Interfaces
+ */
 interface PaymentInfoInterface extends AbstractResourceInterface
 {
-    //['BANK_TRANSFER', 'DIRECT_DEBIT', 'DIRECT_PAYMENT', 'CLEARING', 'CREDIT_CARD_CHARGE', 'FOREIGN_PAYMENT', 'OTHER', 'CASH', 'DOMESTIC_PAYMENT_PLUSGIRO', 'DOMESTIC_PAYMENT_BANKGIRO', 'DOMESTIC_PAYMENT_CREDITOR', 'DKLMPKRE', 'NETS'],
+
+    public const PAYMENT_METHOD_BANK_TRANSFER = 'BANK_TRANSFER';
+    public const PAYMENT_METHOD_DIRECT_DEBIT = 'DIRECT_DEBIT';
+    public const PAYMENT_METHOD_DIRECT_PAYMENT = 'DIRECT_PAYMENT';
+    public const PAYMENT_METHOD_CLEARING = 'CLEARING';
+    public const PAYMENT_METHOD_CREDIT_CARD_CHARGE = 'CREDIT_CARD_CHARGE';
+    public const PAYMENT_METHOD_FOREIGN_PAYMENT = 'FOREIGN_PAYMENT';
+    public const PAYMENT_METHOD_OTHER = 'OTHER';
+    public const PAYMENT_METHOD_CASH = 'CASH';
+    public const PAYMENT_METHOD_DOMESTIC_PAYMENT_PLUSGIRO = 'DOMESTIC_PAYMENT_PLUSGIRO';
+    public const PAYMENT_METHOD_DOMESTIC_PAYMENT_BANKGIRO = 'DOMESTIC_PAYMENT_BANKGIRO';
+    public const PAYMENT_METHOD_DOMESTIC_PAYMENT_CREDITOR = 'DOMESTIC_PAYMENT_CREDITOR';
+    public const PAYMENT_METHOD_DKLMPKRE = 'DKLMPKRE';
+    public const PAYMENT_METHOD_NETS = 'NETS';
+
+    /**
+     * Payment method. One of:
+     * - BANK_TRANSFER
+     * - DIRECT_DEBIT
+     * - DIRECT_PAYMENT
+     * - CLEARING
+     * - CREDIT_CARD_CHARGE
+     * - FOREIGN_PAYMENT
+     * - OTHER
+     * - CASH
+     * - DOMESTIC_PAYMENT_PLUSGIRO
+     * - DOMESTIC_PAYMENT_BANKGIRO
+     * - DOMESTIC_PAYMENT_CREDITOR
+     * - DKLMPKRE
+     * - NETS
+     *
+     * @return string
+     */
     public function getPaymentMethod(): string;
 
-    //Currency of the payment in ISO 4217 format. Example: EUR. ,
+    /**
+     * Currency of the payment in ISO 4217 format. Example: EUR.
+     *
+     * @return string
+     */
     public function getCurrency(): string;
 
-    //Payment reference code. If specified, must be a valid reference code, if not specified the code will be automatically generated. The last digit is a check digit. ,
+    /**
+     * Payment reference code. If specified, must be a valid reference code,
+     * if not specified the code will be automatically generated.
+     * The last digit is a check digit.
+     *
+     * @return null|string
+     */
     public function getBankReferenceCode(): ?string;
 
-    //Payment bank account. Not required if payment method is cash. ,
+    /**
+     * Payment bank account. Not required if payment method is cash.
+     *
+     * @return null|BankAccountInterface
+     */
     public function getBankAccount(): ?BankAccountInterface;
 
-    //Payment due date. ,
+    /**
+     * Payment due date.
+     *
+     * @return DateTime
+     */
     public function getDueDate(): DateTime;
 
-    //Currency exchange rate. Calculated as the amount of one unit of domestic currency in foreign currency. Only foreign currency payments should have a value other than 1.
+    /**
+     * Currency exchange rate.
+     * Calculated as the amount of one unit of domestic currency in foreign currency.
+     * Only foreign currency payments should have a value other than 1.
+     *
+     * @return float
+     */
     public function getCurrencyRate(): float;
 }
--- src/Procountor/Response/PaymentInfo.php
+++ src/Procountor/Response/PaymentInfo.php
@@ -5,38 +5,43 @@ namespace Procountor\Procountor\Response;
 use Procountor\Procountor\Interfaces\BankAccountInterface;
 use Procountor\Procountor\Interfaces\PaymentInfoInterface;
 use DateTime;
-use stdClass;
 
+
+/**
+ * class PaymentInfo
+ *
+ * @see https://dev.procountor.com/api-reference/#model-PaymentInfo
+ *
+ * @package Procountor\Procountor\Response
+ */
 class PaymentInfo extends AbstractResponse implements PaymentInfoInterface
 {
 
-    //['BANK_TRANSFER', 'DIRECT_DEBIT', 'DIRECT_PAYMENT', 'CLEARING', 'CREDIT_CARD_CHARGE', 'FOREIGN_PAYMENT', 'OTHER', 'CASH', 'DOMESTIC_PAYMENT_PLUSGIRO', 'DOMESTIC_PAYMENT_BANKGIRO', 'DOMESTIC_PAYMENT_CREDITOR', 'DKLMPKRE', 'NETS'],
     public function getPaymentMethod(): string
     {
         return $this->data->paymentMethod;
     }
 
-    //Currency of the payment in ISO 4217 format. Example: EUR. ,
     public function getCurrency(): string
     {
         return $this->data->currency;
     }
-    //Payment reference code. If specified, must be a valid reference code, if not specified the code will be automatically generated. The last digit is a check digit. ,
+
     public function getBankReferenceCode(): ?string
     {
         return $this->data->bankReferenceCode;
     }
-    //Payment bank account. Not required if payment method is cash. ,
+
     public function getBankAccount(): ?BankAccountInterface
     {
         return new BankAccount($this->data->bankAccount);
     }
-    //Payment due date. ,
+
     public function getDueDate(): DateTime
     {
         return new DateTime($this->data->dueDate);
     }
-    //Currency exchange rate. Calculated as the amount of one unit of domestic currency in foreign currency. Only foreign currency payments should have a value other than 1.
+
     public function getCurrencyRate(): float
     {
         return $this->data->currencyRate;
