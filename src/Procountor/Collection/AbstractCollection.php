861fe0dd2b15fad5fb1369d820ff769d2e49b91c
Iikka Timlin <iikka@docue.com>
2021-09-28 12:51:51 +0300

    Type safety stuff

--- src/Procountor/Collection/AbstractCollection.php
+++ src/Procountor/Collection/AbstractCollection.php
@@ -6,30 +6,30 @@ use Procountor\Procountor\Interfaces\AbstractResourceInterface;
 use ArrayIterator;
 use IteratorAggregate;
 use Countable;
+use TypeError;
 
 abstract class AbstractCollection implements IteratorAggregate, Countable
 {
-    private $items = [];
+    private array $items = [];
 
-    //PHP 7.2 ONWARDS :)
-    //abstract public function addItem(AbstractResourceInterface $item): AbstractCollection;
+    abstract public function addItem(AbstractResourceInterface $item): AbstractCollection;
 
     public function addItemToCollection($item, $skipcheck = false)
     {
         if (!$skipcheck && !($item instanceof AbstractResourceInterface)) {
-            throw new \Exception('Invalid item');
+            throw new TypeError('Invalid item');
         }
 
         $this->items[] = $item;
         return $this;
     }
 
-    public function getIterator()
+    public function getIterator(): ArrayIterator
     {
         return new ArrayIterator($this->items);
     }
 
-    public function count()
+    public function count(): int
     {
         return $this->getIterator()->count();
     }
