1d37cc136dac7e2a303b6c062cec4ebe9079dffa
Iikka Timlin <iikka@docue.com>
2021-09-28 14:49:43 +0300

    Fix type check

--- src/Procountor/Collection/InvoiceRowCollection.php
+++ src/Procountor/Collection/InvoiceRowCollection.php
@@ -2,12 +2,17 @@
 
 namespace Procountor\Procountor\Collection;
 
+use Procountor\Procountor\Interfaces\AbstractResourceInterface;
 use Procountor\Procountor\Interfaces\InvoiceRowInterface;
+use TypeError;
 
 class InvoiceRowCollection extends AbstractCollection
 {
-    public function addItem(InvoiceRowInterface $item): AbstractCollection
+    public function addItem(AbstractResourceInterface $item): AbstractCollection
     {
+        if (!($item instanceof InvoiceRowInterface)) {
+            throw new TypeError('Invalid type. InvoiceRowInterface expected.');
+        }
         $this->addItemToCollection($item);
         return $this;
     }
